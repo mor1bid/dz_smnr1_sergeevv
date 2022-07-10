9acbbab234dda9fa90bb1eb172aa3de4c95560e3
De <spark49942@gmail.com>
2022-07-10 23:24:53 +0300

    Задание 3

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿array [4, -3, 7];
+﻿int array;
+array [4, -3, 7];
 ind=0;
 while (ind<array.Lenght) {
-if (array[index] % 2) {
-Console.Write(array[index]);
+if (array[ind] % 2) {
+Console.Write(array[ind]);
 ind++;
 }
 }
