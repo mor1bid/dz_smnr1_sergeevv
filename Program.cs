a9f22bc1a872a54a3d9c06e6effd022dcfdbec22
De <spark49942@gmail.com>
2022-07-11 01:25:49 +0300

    Задание 4 == 0

--- Program.cs
+++ Program.cs
@@ -3,20 +3,18 @@ int starta = 1;
 int startb = 1;
 while (starta <= numa)
 {
-    if (numa % 2 == 0) {
+    if (starta % 2 == 0) {
         Console.Write(starta);
         } else {Console.Write("");
-        numa--;
         }
     starta++;
 }
 Console.Write(" AND ");
 while (startb <= numb)
 {
-    if (numb % 2 == 0) {
+    if (startb % 2 == 0) {
     Console.Write(startb);
         } else {Console.Write("");
-        numb--;
         }
     startb++;
 }
