4554dce1e7a8ddaed4d69410f468f197f9016ced
De <spark49942@gmail.com>
2022-07-11 00:16:09 +0300

    Задание 4

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int numa = 5, numb = 8;
+int starta = -numa;
+int startb = -numb;
+while (starta <= numa);
+{
+    Console.Write(starta);
+    start++;
+    Console.Write(" ; ");
+}
+
+while (startb <= numb);
+{
+    Console.Write(startb);
+    start++;
+}
