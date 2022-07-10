f62ba63b161850147b938ab2e1b7ebc1ac210cee
De <spark49942@gmail.com>
2022-07-10 21:09:01 +0300

    Задание 1

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ Console.Write(max1);
 Console.Write(" больше числа ");
 Console.Write(min1);
 Console.Write("; ");
-if (a2<b2) { max2=a2; min2=b2; }
+if (a2>b2) { max2=a2; min2=b2; }
 else { max2=b2; min2=a2; }
 Console.Write("Число ");
 Console.Write(max2);
 Console.Write(" больше числа ");
 Console.Write(min2);
 Console.Write("; ");
-if (a3<b3) { max3=a3; min3=b3; }
+if (a3>b3) { max3=a3; min3=b3; }
 else { max3=b3; min3=a3; }
 Console.Write("Число ");
 Console.Write(max3);
