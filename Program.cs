827dfa2ed9e6aff70122eb72f9af3982aa513d45
De <spark49942@gmail.com>
2022-07-10 22:07:46 +0300

    Anynumber bugs

--- Program.cs
+++ Program.cs
@@ -1,22 +1,14 @@
-﻿int a1=15, b1=7, a2=22, b2=10, a3=-9, b3=-3, max1, min1, max2, min2, max3, min3;
-if (a1>b1) { max1=a1; min1=b1; }
-else { max1=b1; min1=a1; }
+﻿ulong index=999999999999999, count=0, a, b, max, min;
+while (count<=index) {
+Console.WriteLine("Введите два числа:");
+a=Convert.ToUInt32(Console.ReadLine());
+b=Convert.ToUInt32(Console.ReadLine());
+if (a>b) { max=a; min=b; }
+else { max=b; min=a; }
 Console.Write("Число ");
-Console.Write(max1);
+Console.Write(max);
 Console.Write(" больше числа ");
-Console.Write(min1);
+Console.Write(min);
 Console.Write("; ");
-if (a2>b2) { max2=a2; min2=b2; }
-else { max2=b2; min2=a2; }
-Console.Write("Число ");
-Console.Write(max2);
-Console.Write(" больше числа ");
-Console.Write(min2);
-Console.Write("; ");
-if (a3>b3) { max3=a3; min3=b3; }
-else { max3=b3; min3=a3; }
-Console.Write("Число ");
-Console.Write(max3);
-Console.Write(" больше числа ");
-Console.Write(min3);
-Console.Write("; ");
+count++;
+}
