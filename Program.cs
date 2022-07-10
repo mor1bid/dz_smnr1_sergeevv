00888ee1fe62649ebd6e61d5359a18ce8ad75139
De <spark49942@gmail.com>
2022-07-11 01:35:20 +0300

    Задание 4 starta%2

--- Program.cs
+++ Program.cs
@@ -1,21 +1,22 @@
 ﻿int numa = 5, numb = 8;
 int starta = 1;
 int startb = 1;
+Console.Write("Чётные числа от одного до первого числа: ");
 while (starta <= numa)
 {
     if (starta % 2 == 0) {
         Console.Write(starta);
-        Console.Write(" , ");
+        Console.Write(", ");
         } else {Console.Write("");
         }
     starta++;
 }
-Console.Write(" AND ");
+Console.Write("И от одного до второго числа: ");
 while (startb <= numb)
 {
     if (startb % 2 == 0) {
     Console.Write(startb);
-    Console.Write(" , ");
+    Console.Write(", ");
         } else {Console.Write("");
         }
     startb++;
